5ebd8e19cb0f7048ef06d189babe2f4c2d43e994
ShortCoursesICT <ict.cet.shortcourses@gmail.com>
2022-03-21 16:04:11 +0800

    if statements no need test

---
+++ if.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	number1 := 17
+	number2 := 24
+	resultMessage := "No outcome."
+	//Insert your code here
+	//Hint: You may wish to make use of strconv.Itoa to convert int to string
+
+}
